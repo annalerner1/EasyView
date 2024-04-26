c3d9a6b7aa0e80ddd9794e81fd63a7bff7daf2cb
annalerner1 <annalerner@uchicago.edu>
2024-04-26 18:55:42 -0500

    Initial Commit

---
+++ EasyInterview/EasyInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyInterviewApp.swift
+//  EasyInterview
+//
+//  Created by Anna Lerner on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
